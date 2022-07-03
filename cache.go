d5996cb3a58fdde6690cf700942d7c5e319dc14b
Ayan Akkassov <akkassov.ayan@gmail.com>
2022-07-03 20:42:13 +0600

    Add implemented cache.go file

--- cache.go
+++ cache.go
@@ -1,23 +1,66 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+type cacheValue struct {
+	value    string
+	deadline time.Time
+}
 
 type Cache struct {
+	cacheMap map[string]cacheValue
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cacheMap: map[string]cacheValue{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	c.deleteExpiredKeys()
+	if val, ok := c.cacheMap[key]; ok {
+		return val.value, ok
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	cv := cacheValue{value: value, deadline: time.Time{}}
+	c.cacheMap[key] = cv
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	c.deleteExpiredKeys()
+	keys := make([]string, 0)
+	for key, _ := range c.cacheMap {
+		keys = append(keys, key)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	if deadline.IsZero() {
+		deadline = time.Now()
+	}
+	cv := cacheValue{value, deadline}
+	c.cacheMap[key] = cv
+}
+
+func (c *Cache) deleteExpiredKeys() {
+	expiredKeys := make([]string, 0)
+	for key, value := range c.cacheMap {
+		if value.deadline.IsZero() {
+			continue
+		}
+		if value.deadline.After(time.Now()) {
+			expiredKeys = append(expiredKeys, key)
+		}
+	}
+
+	for _, key := range expiredKeys {
+		delete(c.cacheMap, key)
+	}
 }
